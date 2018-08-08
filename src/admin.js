b9636490cb225262fcd7ab5a7063c991cb4ffa97
zhenxianyimeng <1920405993@qq.com>
2018-08-08 15:26:28 +0800

    左右上下布局

---
+++ src/admin.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Row, Col } from 'antd'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import NavLeft from './components/NavLeft'
+export default class Admin extends React.Component{
+
+    render(){
+        return (
+            <Row>
+                <Col span="3">
+                    <NavLeft>NavLeft</NavLeft>
+                </Col>
+                <Col span="21">
+                    <Header>Header</Header>
+                    <Row>
+                        Content
+                    </Row>
+                    <Footer>Footer</Footer>
+                </Col>
+            </Row>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './pages/demo/Life.js';
+import Admin from './admin';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Admin />, document.getElementById('root'));
 registerServiceWorker();
