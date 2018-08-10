f0f97befb8855935b8696021cbb8fea1683918b8
zhenxianyimeng <1920405993@qq.com>
2018-08-10 10:22:29 +0800

    add menu

--- src/admin.js
+++ src/admin.js
@@ -3,17 +3,18 @@ import { Row, Col } from 'antd'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import NavLeft from './components/NavLeft'
+import './admin.less'
 export default class Admin extends React.Component{
 
     render(){
         return (
-            <Row>
-                <Col span="3">
+            <Row className="container">
+                <Col span="4" className="nav-left">
                     <NavLeft>NavLeft</NavLeft>
                 </Col>
-                <Col span="21">
+                <Col span="20" className="main">
                     <Header>Header</Header>
-                    <Row>
+                    <Row className="content">
                         Content
                     </Row>
                     <Footer>Footer</Footer>
--- src/components/NavLeft/index.js
+++ src/components/NavLeft/index.js
@@ -1,11 +1,38 @@
 import React from 'react'
+import {Menu, Icon} from 'antd';
+import './index.less'
+//
+// const MenuItemGroup = Menu.ItemGroup;
+import MenuConfig from '../../config/menuConfig'
 
-export default class NavLeft extends React.Component{
+export default class NavLeft extends React.Component {
 
-    render(){
+    componentWillMount(){
+
+    }
+
+    //菜单渲染
+    renderMenu = ()=>{
+
+    }
+
+    render() {
+        const SubMenu = Menu.SubMenu;
+
         return (
             <div>
-                This is a NavLeft
+                <div className="logo">
+                    <img src="/asserts/logo-ant.svg" alt=""/>
+                    <h1>Imooc MS</h1>
+                </div>
+                <Menu theme="dark">
+                    <SubMenu key="sub1" title={<span><Icon type="mail"/><span>Navigation One</span></span>}>
+                        <Menu.Item key="1">Option 1</Menu.Item>
+                        <Menu.Item key="2">Option 2</Menu.Item>
+                        <Menu.Item key="3">Option 3</Menu.Item>
+                        <Menu.Item key="4">Option 4</Menu.Item>
+                    </SubMenu>
+                </Menu>
             </div>
         );
     }
