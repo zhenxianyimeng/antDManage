2049b94ae82c113f411161e68c5e96c3be56c241
zhenxianyimeng <1920405993@qq.com>
2018-08-24 23:41:44 +0800

    轮播图

--- src/router.js
+++ src/router.js
@@ -11,6 +11,8 @@ import Notice from './pages/ui/notice'
 import Messages from './pages/ui/messages'
 import Tabs from './pages/ui/tabs'
 import Gallery from "./pages/ui/gallery";
+import Carousel from './pages/ui/carousel';
+
 export default class IRouter extends React.Component{
 
 	render(){
@@ -30,6 +32,7 @@ export default class IRouter extends React.Component{
 									<Route path="/admin/ui/messages" component={Messages}/>
 									<Route path="/admin/ui/tabs" component={Tabs}/>
 									<Route path="/admin/ui/gallery" component={Gallery}/>
+									<Route path="/admin/ui/carousel" component={Carousel}/>
 
 									<Route component={NoMatch}/>
 								</Switch>
