1af0ba01760f6dd371481437f3ee155b30178c85
真仙忆梦 <1920405993@qq.com>
2018-09-01 17:30:38 +0800

    简单表格

--- src/pages/table/basicTable.js
+++ src/pages/table/basicTable.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Card, Table} from 'antd';
+import {Card, Table, Modal, Button, message} from 'antd';
 import axios from './../../axios/index';
 
 export default class BasicTable extends React.Component{
@@ -7,6 +7,34 @@ export default class BasicTable extends React.Component{
 	state={
 		dataSource2 : [],
 	}
+
+	handleDelete = () => {
+		let rows = this.state.selectedRows;
+		let ids = [];
+		rows.map((item) => {
+			ids.push(item.id);
+		})
+		Modal.confirm({
+			title:'删除提示',
+			content:`你确定要删除么?${ids.join(',')}`,
+			onOk:() => {
+				message.success("删除成功");
+				this.request();
+			}
+		})
+	}
+
+	onRowClick = (record, index) =>{
+		let selectKey = [index];
+		Modal.info({
+			title:'信息',
+			content:JSON.stringify(record),
+		})
+		this.setState({
+			selectedRowKeys: selectKey,
+			selectedItem: record,
+		})
+    }
 
 	componentDidMount(){
 		const dataSource=[
@@ -41,6 +69,9 @@ export default class BasicTable extends React.Component{
 				time:'09:00'
 			}
 		]
+		dataSource.map((item, index) =>{
+			item.key = index;
+		});
 
 		this.setState({
 			dataSource
@@ -62,22 +93,19 @@ export default class BasicTable extends React.Component{
 			}
 		}).then((res) => {
 			if(res.code == 0){
+				res.result.map((item, index) => {
+					item.key = index;
+				})
 				this.setState({
 					dataSource2: res.result,
+					selectedRows:null,
+					selectedRowKeys:[],
 				})
 			}
 		})
 
-		// let baseUrl = 'https://www.easy-mock.com/mock/5b88888389484103289808e1/api'
-		// axios.get(baseUrl + '/table/list').then(res=>{
-		// 	if(res.status == '200'){
-		// 		this.setState({
-		// 			dataSource2:res.data.result
-		// 		})
-		// 	}
-		// 	// console.log(JSON.stringify(res));
-		// })
-	}
+	};
+
 
 	render(){
 		const columns = [
@@ -91,15 +119,41 @@ export default class BasicTable extends React.Component{
 			},
 			{
 				title:'性别',
-				dataIndex: 'sex'
+				dataIndex: 'sex',
+				render(sex){
+					return sex==1? '男':'女';
+				}
 			},
 			{
 				title:'状态',
-				dataIndex: 'state'
+				dataIndex: 'state',
+				render(state){
+					let config = {
+						'1':'青铜',
+						'2':'白银',
+						'3':'黄金',
+						'4':'铂金',
+						'5':'砖石'
+					}
+					return config[state];
+				}
 			},
 			{
 				title:'爱好',
-				dataIndex: 'interest'
+				dataIndex: 'interest',
+				render(interest){
+					let config = {
+						'1':'游泳',
+						'2':'篮球',
+						'3':'足球',
+						'4':'跑步',
+						'5':'篮球',
+						'6':'骑行',
+						'7':'徒步',
+						'8':'旅游',
+					}
+					return config[interest];
+				}
 			},
 			{
 				title:'生日',
@@ -114,7 +168,31 @@ export default class BasicTable extends React.Component{
 				dataIndex: 'time'
 			},
 
-		]
+		];
+
+		const {selectedRowKeys} = this.state;
+
+		const rowSelection = {
+			type: 'radio',
+			selectedRowKeys,
+		};
+
+		const rowCheckSelection = {
+			type: 'check',
+			selectedRowKeys,
+			onChange:(selectedRowKeys, selectedRows)=>{
+				let ids = [];
+				selectedRows.map((item)=>{
+					ids.push(item.id)
+				})
+				this.setState({
+					selectedRowKeys,
+					selectedRows,
+				})
+			}
+		};
+
+
 
 		return(
 			<div>
@@ -128,6 +206,41 @@ export default class BasicTable extends React.Component{
 				</Card>
 				<Card title="动态数据渲染表格" style={{margin: '10px 0'}}>
 					<Table
+						bordered
+						columns={columns} dataSource={this.state.dataSource2}
+						pagination={false}
+					>
+					</Table>
+				</Card>
+				<Card title="单选" style={{margin: '10px 0'}}>
+					<Table
+						onRow={(record, index) => {
+							return {
+								onClick: () => {
+									this.onRowClick(record, index)
+								},       // 点击行
+							};
+						}}
+						rowSelection={rowSelection}
+						bordered
+						columns={columns} dataSource={this.state.dataSource2}
+						pagination={false}
+					>
+					</Table>
+				</Card>
+				<Card title="复选" style={{margin: '10px 0'}}>
+					<div style={{marginBottom: 10}}>
+						<Button onClick={this.handleDelete}>删除</Button>
+					</div>
+					<Table
+						// onRow={(record, index) => {
+						// 	return {
+						// 		onClick: () => {
+						// 			this.onRowClick(record, index)
+						// 		},       // 点击行
+						// 	};
+						// }}
+						rowSelection={rowCheckSelection}
 						bordered
 						columns={columns} dataSource={this.state.dataSource2}
 						pagination={false}
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -3,7 +3,7 @@ export default {
         if(!time)
             return '';
         let date = new Date(time);
-        console.log(date)
+        // console.log(date)
         // console.log(date.)
         return date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate()
         +" "+ date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
