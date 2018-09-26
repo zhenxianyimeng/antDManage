b19664d9fbc822bd6a98df872eaa1649a0aba366
zhenxianyimeng <1920405993@qq.com>
2018-09-26 11:15:12 +0800

    精简菜单

--- src/pages/permission/index.js
+++ src/pages/permission/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {Card, Button, Modal,Select, Form, Input, Radio, Tree} from 'antd';
+import {Card, Button, Modal,Select, Form, Input, Radio, Tree, Transfer} from 'antd';
 import Etable from './../../components/ETable';
 import ETable from "../../components/ETable/index";
 import Utils from '../../utils/utils';
 import axios from '../../axios';
-import menuConfig from '../../resource/menuConfig';
+import menuConfig from '../../config/menuConfig';
 
 const RadioGroup = Radio.Group;
 const FormItem = Form.Item;
@@ -27,6 +27,82 @@ export default class PermissionUser extends React.Component {
 		this.setState({
 			isRoleVisible:true
 		})
+	}
+
+
+	handleUserSubmit = ()=> {
+		let data={}
+		data.user_ids = this.state.targetKeys;
+		data.role_id = this.state.selectedItem.id;
+		axios.ajax({
+			url:'/role/user_role_edit',
+			data:{
+				params:{
+					...data
+				}
+			}
+		}).then(res=>{
+			if(res){
+				this.setState({
+					isUserVisible: false
+				});
+				axios.requestList(this, "/role/list",{});
+			}
+		})
+	}
+
+	handleUserAuth = ()=> {
+		let item = this.state.selectedItem;
+		if(!item){
+			Modal.info({
+				title: '信息',
+				content: '请选择一个角色'
+			})
+			return;
+		}
+		this.setState({
+			isUserVisible:true,
+			detailInfo:item,
+		})
+		this.getRoleUserList(item.id);
+	}
+
+	getRoleUserList = (id)=> {
+		axios.ajax({
+			url:'/role/user_list',
+			data:{
+				params:{
+					id
+				}
+			}
+		}).then((res)=>{
+			if(res){
+				this.getAuthUserList(res.result);
+			}
+		})
+	}
+
+	getAuthUserList = (dataSource)=> {
+		const mockData = [];
+		const targetKeys = [];
+		if(dataSource && dataSource.length > 0){
+			for(let i=0; i<dataSource.length; i++){
+				const data = {
+					key: dataSource[i].user_id,
+					title: dataSource[i].user_name,
+					status:dataSource[i].status,
+				}
+				if(data.status == 1){
+					targetKeys.push(data.key);
+				}
+				mockData.push(data);
+
+			}
+			this.setState({
+				mockData,
+				targetKeys
+			})
+		}
 	}
 
 	handlePermission = ()=>{
@@ -122,7 +198,7 @@ export default class PermissionUser extends React.Component {
 				<Card>
 					<Button type="primary" onClick={this.handleRole}>创建角色</Button>
 					<Button type="primary" onClick={this.handlePermission}>设置权限</Button>
-					<Button type="primary">用户授权</Button>
+					<Button type="primary" onClick={this.handleUserAuth}>用户授权</Button>
 				</Card>
 				<div className="content-wrap">
 					<ETable
@@ -161,6 +237,29 @@ export default class PermissionUser extends React.Component {
 						patchMenuInfo={(checkedKeys)=>{
 							this.setState({
 								menuInfo: checkedKeys,
+							})
+						}}
+					/>
+				</Modal>
+
+				<Modal title="用户授权"
+				       visible={this.state.isUserVisible}
+				       width={800}
+				       onOk={this.handleUserSubmit}
+				       onCancel={()=>{
+					       this.setState({
+						       isUserVisible: false,
+					       })
+				       }}
+				>
+					<RoleAuthForm
+						wrappedComponentRef={(inst)=> this.userAuthForm = inst}
+						detailInfo={this.state.detailInfo}
+						targetKeys={this.state.targetKeys}
+						mockData={this.state.mockData}
+						patchUserInfo={(targetKeys)=>{
+							this.setState({
+								targetKeys
 							})
 						}}
 					/>
@@ -274,3 +373,67 @@ class PermEditForm extends React.Component{
 	}
 }
 PermEditForm = Form.create({})(PermEditForm);
+
+class RoleAuthForm extends React.Component{
+
+	onCheck = (checkedKeys)=> {
+		this.props.patchMenuInfo(checkedKeys);
+	}
+
+	renderTreeNode = (data)=>{
+		return data.map((item)=>{
+			if(item.children){
+				return(<TreeNode title={item.title} key={item.key}>
+					{this.renderTreeNode(item.children)}
+				</TreeNode>)
+
+			}else {
+				return <TreeNode title={item.title} key={item.key}/>
+			}
+		})
+	}
+
+	filterOption = (inputValue, option) => {
+		return option.title.indexOf(inputValue) > -1;
+	}
+
+	handleChange = (targetKeys)=> {
+		this.props.patchUserInfo(targetKeys);
+	}
+
+	render(){
+		const formItemLayout = {
+			labelCol: {span: 5},
+			wrapperCol: {span: 19}
+		}
+
+		const {getFieldDecorator} = this.props.form;
+
+		const detail_info = this.props.detailInfo;
+		const menuInfo = this.props.menuInfo;
+
+		return(
+			<Form layout="horizontal">
+				<FormItem label="角色名称" {...formItemLayout}>
+					<Input disabled placeholder={detail_info.role_name} >
+
+					</Input>
+				</FormItem>
+				<FormItem label="选择用户"  {...formItemLayout}>
+					<Transfer
+						listStyle={{width:200, height:400}}
+						dataSource={this.props.mockData}
+						titles={['待选用户','已选用户']}
+						showSearch
+						searchPlaceholder='请输入用户名'
+						filterOption={this.filterOption}
+						targetKeys={this.props.targetKeys}
+						onChange={this.handleChange}
+						render={item=>item.title}
+					/>
+				</FormItem>
+			</Form>
+		);
+	}
+}
+RoleAuthForm = Form.create({})(RoleAuthForm);
--- src/router.js
+++ src/router.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import {HashRouter, Route, Switch} from 'react-router-dom'
+import {HashRouter, Route, Switch, Redirect} from 'react-router-dom'
 import App from './App'
 import Login from './pages/login'
 import Admin from './admin'
+import Home from './pages/home'
 import Buttons from './pages/ui/buttons'
 import NoMatch from './pages/nomatch'
 import Modals from './pages/ui/modals'
@@ -35,42 +36,45 @@ export default class IRouter extends React.Component{
 		return(
 			<HashRouter>
 				<App>
+					<Switch>
 					<Route path="/login" component={Login}></Route>
-					<Route path="/admin" render={
+					<Route path="/common"  render={()=>
+						<Common>
+							<Route path="/common/order/detail/:orderId"  component={OrderDetail}/>
+						</Common>
+					}/>
+					<Route path="/" render={
 						()=>
 							<Admin>
 								<Switch>
-									<Route path="/admin/ui/buttons" component={Buttons}></Route>
-									<Route path="/admin/ui/modals" component={Modals}/>
-									<Route path="/admin/ui/loadings" component={Loadings}/>
-									<Route path="/admin/ui/notification" component={Notice}/>
-									<Route path="/admin/ui/messages" component={Messages}/>
-									<Route path="/admin/ui/tabs" component={Tabs}/>
-									<Route path="/admin/ui/gallery" component={Gallery}/>
-									<Route path="/admin/ui/carousel" component={Carousel}/>
-									<Route path="/admin/form/login" component={FormLogin}/>
-									<Route path="/admin/form/reg" component={FormRegister}/>
-									<Route path="/admin/table/basic" component={BasicTable}/>
-									<Route path="/admin/table/high" component={HighTable}/>
-									<Route path="/admin/city" component={City}/>
-									<Route path="/admin/order" component={Order}/>
-									<Route path="/admin/user" component={User}/>
-									<Route path="/admin/bikeMap" component={BikeMap}/>
-									<Route path="/admin/charts/bar" component={Bar}/>
-									<Route path="/admin/charts/pie" component={Pie}/>
-									<Route path="/admin/charts/line" component={Line}/>
-									<Route path="/admin/rich" component={RichText}/>
-									<Route path="/admin/permission" component={Permission}/>
-
+									<Route path="/home" component={Home}></Route>
+									<Route path="/ui/buttons" component={Buttons}></Route>
+									<Route path="/ui/modals" component={Modals}/>
+									<Route path="/ui/loadings" component={Loadings}/>
+									<Route path="/ui/notification" component={Notice}/>
+									<Route path="/ui/messages" component={Messages}/>
+									<Route path="/ui/tabs" component={Tabs}/>
+									<Route path="/ui/gallery" component={Gallery}/>
+									<Route path="/ui/carousel" component={Carousel}/>
+									<Route path="/form/login" component={FormLogin}/>
+									<Route path="/form/reg" component={FormRegister}/>
+									<Route path="/table/basic" component={BasicTable}/>
+									<Route path="/table/high" component={HighTable}/>
+									<Route path="/city" component={City}/>
+									<Route path="/order" component={Order}/>
+									<Route path="/user" component={User}/>
+									<Route path="/bikeMap" component={BikeMap}/>
+									<Route path="/charts/bar" component={Bar}/>
+									<Route path="/charts/pie" component={Pie}/>
+									<Route path="/charts/line" component={Line}/>
+									<Route path="/rich" component={RichText}/>
+									<Route path="/permission" component={Permission}/>
+									<Redirect to="/home"/>
 									<Route component={NoMatch}/>
 								</Switch>
 							</Admin>
 					} />
-					<Route path="/common" render={()=>
-						<Common>
-							<Route path="/common/order/detail/:orderId" component={OrderDetail}/>
-						</Common>
-					}/>
+					</Switch>
 				</App>
 			</HashRouter>
 		);
