15b3ded9666ed61ddc6097976305d6d331fe101d
zhenxianyimeng <1920405993@qq.com>
2018-09-27 23:40:46 +0800

    引入redux

--- src/index.js
+++ src/index.js
@@ -4,8 +4,15 @@ import './index.css';
 import App from './pages/demo/Life.js';
 import Admin from './admin';
 import Router from './router';
-
+import { Provider } from 'react-redux';
+import configureStore from './redux/store/configureStore'
 import registerServiceWorker from './registerServiceWorker';
+const store = configureStore();
 
-ReactDOM.render(<Router />, document.getElementById('root'));
+ReactDOM.render(
+	<Provider >
+		<Router store={store}/>
+	</Provider>
+	, document.getElementById('root'));
+
 registerServiceWorker();
